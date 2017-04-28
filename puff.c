60e5e7caba0c81e616cf7373bc52492c557958bc
Sarah Depew <sdepew@brynmawr.edu>
2017-04-28 18:07:07 -0400

    Checking my Huff and Puff against His

--- puff.c
+++ puff.c
@@ -3,22 +3,8 @@
 #include "puff.h"
 
 /*
-
-To decompress a file:
-
-Read in the table of character frequencies in the first 256 ints in the input file.
-
-Build the Huffman tree (as in steps 2-4 above).
-
-For every bit read in from the input file, traverse down the Huffman tree (going left 
-for 0 bits and right for 1 bits). When you reach a leaf, there will be a character stored in the leaf node. 
-Write this character to the output file.
-
-Repeat step 3 for every character in the input file (as determined by looking at the frequency 
-of the root of the Huffman tree, which is necessarily the total number of characters in the input).
-
-We’re done. Close the files and free your memory.
-
+Name: Sarah Depew
+Resources: Professor, TA, Kennedy, Trista, Ruby, Sarah K., Amanda, and textbook (I know I did some web research, but I do not distinctly remember implementing details from the resources, but I acknowledge them anyway)
 */
 
 int main(int argc, char *argv[]){
@@ -36,7 +22,6 @@ int main(int argc, char *argv[]){
 	//there was an error in opening the file
 	if(input==NULL||output==NULL){
 		printf("Sorry, there was an error with opening one of the files.\n"); 
-		//fclose(input); 
 		return -1; 
 	}
 	
@@ -75,15 +60,8 @@ int main(int argc, char *argv[]){
 		num--; 
 	}
 		
-	/*
-	For every bit read in from the input file, traverse down the Huffman tree (going left 
-	for 0 bits and right for 1 bits). When you reach a leaf, there will be a character stored in the leaf node. 
-	Write this character to the output file.
-
-	Repeat step 3 for every character in the input file (as determined by looking at the frequency 
-	of the root of the Huffman tree, which is necessarily the total number of characters in the input).
-	*/
-	
+	//read the characters from the input file 
+		
 	//current character read from the file :)
 	int current; 
 	int chars_read = 0; 
@@ -91,33 +69,23 @@ int main(int argc, char *argv[]){
 	int values_read = 0;
 	int num_bits_gotten = 0; 
 	
-	//make an array to store all the bytes
-	int bits[8]={};  
-	
-	//rewind(input); 
-	//current = fgetc(input);
-	
 	current = fgetc(input);
 	
-	
+	//read and print out the codes
 	while(current!=EOF&&chars_read<leaves[0]->freq){
-		for(int i=0; i<8&&chars_read<leaves[0]->freq; i++){
+		for(int i=0; i<8&&chars_read<leaves[0]->freq&&current_node!=NULL; i++){
 			int current_bit = get_bit(&values_read, &current);
-			//printf("%d", current_bit); 
-			//printf("\n");
 			num_bits_gotten++; 
 		
-			if(current_bit==0){
+			if(current_node!=NULL && current_bit==0){
 				current_node = current_node->left;
-				//printf("%c", current_node->ch);
 			}
 			
-			else if(current_bit==1){
+			else if(current_node!=NULL && current_bit==1){
 				current_node = current_node->right;
 			}
 			
-			if(current_node->ch>0){
-				//printf("%c", current_node->ch);
+			if(current_node!=NULL && current_node->ch>0){
 				fprintf(output,"%c", current_node->ch);
 				current_node = leaves[0]; //reset to start traversing tree, again and get next instruction 
 				chars_read++; 
@@ -127,78 +95,6 @@ int main(int argc, char *argv[]){
 		current = fgetc(input);
 	}
 		
-		/*
-		while(num_bits_gotten%8!=0&&num_bits_gotten>0){
-			int current_bit = get_bit(&values_read, &current); 
-			//printf("%d", get_bit(&values_read, &current)); 
-			printf("%d", current_bit); 
-			printf("\n");
-			num_bits_gotten++; 
-		}
-	
-		//reset values after reading
-		values_read = 0;  
-		current = fgetc(input);
-	}
-	
-	
-	while(current!=EOF){
-		//printf("%d\n", current); 
-		
-			while(current_node->ch<0 && current!=EOF){
-			//printf("%d\n", current_node->ch);
-			  
-			if(current==0){
-				current_node = current_node->left;
-				//current = fgetc(input);  
-			}
-		
-			else if(current==1){
-				current_node = current_node->right;
-				//current = fgetc(input);  
-			}
-			current = fgetc(input);
-			//fread(&current, sizeof(int), 1, input);
-			printf("%d", current); 
-		}
-		fprintf(output,"%c", current_node->ch);
-		//printf("%c\n", current_node->ch);
-		
-		current_node = leaves[0]; //reset to start traversing tree, again and get next instruction :)
-		
-		printf("\n"); 
-		values_read = 0; 
-		
-		//get new character
-		current = fgetc(input);	
-	}
-	
-	while(current_node->ch<0 && current!=EOF){
-			//printf("%d\n", current_node->ch);
-			  
-			if(current==0){
-				current_node = current_node->left;
-				//current = fgetc(input);  
-			}
-		
-			else if(current==1){
-				current_node = current_node->right;
-				//current = fgetc(input);  
-			}
-			current = fgetc(input);
-			//fread(&current, sizeof(int), 1, input);
-			printf("%d", current); 
-		}
-		fprintf(output,"%c", current_node->ch);
-		//printf("%c\n", current_node->ch);
-		
-		current_node = leaves[0]; //reset to start traversing tree, again and get next instruction :)
-
-		printf("%d\n", current); 
-		
-	}
-	*/
-	
 	//close the files and free the memory
 	free_memory(leaves[0]); 
 }
@@ -232,41 +128,14 @@ huff_tree_node* get_smallest(huff_tree_node *leaves[NUMCHARS], int *array_size){
 	return smallest_leaf; 
 }
 
-//for(int i=0; i<leaves[0]->freq; i++){
-	/*
-		//printf("%d", current); 
-		while(current_node->ch<0 && current!=EOF){
-			//printf("%d\n", current_node->ch);
-			  
-			if(current==0){
-				current_node = current_node->left;
-				//current = fgetc(input);  
-			}
-		
-			else if(current==1){
-				current_node = current_node->right;
-				//current = fgetc(input);  
-			}
-			current = fgetc(input);
-			//fread(&current, sizeof(int), 1, input);
-			printf("%d", current); 
-		}
-		fprintf(output,"%c", current_node->ch);
-		//printf("%c\n", current_node->ch);
-		
-		current_node = leaves[0]; //reset to start traversing tree, again and get next instruction :)
-		*/
-		
+//function to get the bit from the current character gotten from the file
 int get_bit(int *bits_read, int *current){
-	unsigned char return_value = *current; 
+	unsigned char return_value = *current;
+	 
 	return_value = *current<<(*bits_read); 
-	//printf("Return Val: %d\n", return_value);
-	//printf("Bits read: %d\n", *bits_read); 
 	return_value = return_value>>(7); 
-	
 	*bits_read = (*bits_read)+=1; 
-	//printf("Return Val: %d", return_value); 
-	//printf("%d", *bits_read); 
+
 	return return_value; 
 }
 
--- puff.h
+++ puff.h
@@ -3,8 +3,16 @@
 #include <stdio.h>
 #include "huff_tree_node.h"
 
+/*
+Name: 
+Resources: 
+*/
+
+//get the smallest leaf from the array and need to decrement size of array outside!! otherwise function malfunctions
 huff_tree_node* get_smallest(huff_tree_node *leaves[NUMCHARS], int *array_size); 
 
+//function to get the bit from the current character gotten from the file
 int get_bit(int *bits_read, int *current); 
 
+//Frees all the memory used in this part of the program
 void free_memory(huff_tree_node *root);  
